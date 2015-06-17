6cde57342f45859dfc44f0b4e51aa0f437b72b2d
Mathias Kaufmann <me@stei.gr>
2015-06-17 14:01:42 +0200

    Update installer and cli

--- docker/files/usr/lib/cluster-element/cli.rb
+++ docker/files/usr/lib/cluster-element/cli.rb
@@ -1,8 +1,15 @@
 require "thor"
+require "securerandom"
+require "fileutils"
 
 module ClusterElement
   class Cli < Thor
     desc "cmd","Command Management"
+    def update
+      t=SecureRandom.hex(8)
+      repo="https://github.com/steigr/cluster-element.git"
+      puts `git clone #{repo} #{t}; rsync -r #{t}/docker/files /`.strip
+    end
     def cmd
       puts "Doing cmd stuff"
     end
