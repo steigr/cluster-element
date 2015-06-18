15518a4d41e9abeaeea0c39924558ffbdaef852e
Mathias Kaufmann <me@stei.gr>
2015-06-18 14:34:05 +0200

    Typo

--- docker/files/usr/lib/cluster-element/config.rb
+++ docker/files/usr/lib/cluster-element/config.rb
@@ -189,6 +189,7 @@ module ClusterElement
       when "%private_ipv4" then ClusterElement::Network.private_ipv4
       when "%public_ipv4"  then ClusterElement::Network.public_ipv4
       when "%etcd_self"    then "127.0.0.1:2379"
+      when "%cluster"      then serf[:discover]
       else var
       end
     end
